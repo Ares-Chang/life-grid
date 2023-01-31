b6dbf7bdc4235a3487744536610d620f7dccd712
chang <2622310479@qq.com>
2023-01-31 17:19:16 +0800

    feat: create monthly calendar grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup lang="ts">
+const num = 900
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
+  <div flex justify-center>
+    <div max-w-1200px p-6 flex="~ wrap" gap-2>
+      <div
+        v-for="item in num" :key="item"
+        w-6 h-6
+        bg-pink
+      />
+    </div>
   </div>
 </template>
