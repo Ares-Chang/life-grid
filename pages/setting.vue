12073790d324e5b36d0444a4bcc34498b32695bb
chang <2622310479@qq.com>
2023-02-01 10:07:40 +0800

    feat: add settings

---
+++ pages/setting.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div w-md m-auto text-center>
+    <h2 text-3xl text-left font-bold>
+      Settings
+    </h2>
+    <input
+      placeholder="Your date of birth?"
+      type="date" autocomplete="off"
+      p="x-4 y-2" my-6 w-full
+      text-center bg-transparent
+      border="~ rd gray-200 dark:gray-700"
+      outline-none
+    >
+  </div>
+</template>
