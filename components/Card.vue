0f7fdc56fb7bbb7d109c3877acd2269af325442b
chang <2622310479@qq.com>
2023-02-04 17:21:50 +0800

    feat: Card 组件判断是否传入 title 动态控制显隐

--- components/Card.vue
+++ components/Card.vue
@@ -1,12 +1,15 @@
+<script setup lang="ts">
+const isTitle = !!useSlots().title // 判断 title 是否传入 slot
+</script>
+
 <template>
   <div p-8 my-8 border rd-2 relative>
     <div
+      v-if="isTitle"
       p="x-4 y-2" bg-blue rd-2
       absolute top--4
     >
-      <slot name="title">
-        Tisp
-      </slot>
+      <slot name="title" />
     </div>
     <slot />
   </div>
