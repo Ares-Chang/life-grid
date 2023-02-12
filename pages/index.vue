f8f34a778fd6e7f4f4556cf17c44e3551d4b8b08
chang <2622310479@qq.com>
2023-02-12 20:56:20 +0800

    feat: 当前时间高亮闪烁

--- pages/index.vue
+++ pages/index.vue
@@ -15,9 +15,29 @@ const init = new InitGrid(config)
       <div
         v-for="(_, _index) in group" :key="_index"
         w-6 h-6
-        :class="init.tipList[index].color"
+        :class="[
+          init.tipList[index].color,
+          (index === 0 && group === _index + 1) && 'animate-flicker',
+        ]"
       />
     </template>
   </div>
   <Tips :list="init.tipList" />
 </template>
+
+<style>
+@keyframes flicker {
+  from,
+  20%,
+  90%,
+  to {
+    @apply op-100
+  }
+  50% {
+    @apply op-20
+  }
+}
+.animate-flicker {
+  animation: flicker 3s linear infinite;
+}
+</style>
